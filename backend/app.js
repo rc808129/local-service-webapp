417ca528496a26b5cb26a72e9973631dde12721f
rc808129 <rc808129@gmail.com>
2026-04-20 13:41:58 +0530

    updated code

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,9 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
-    'http://127.0.0.1:5174'
+    'http://127.0.0.1:5174',
+    'https://local-service-webapp.vercel.app'
+
   ],
   methods: ['GET', 'POST', 'PUT',
     'PATCH', 'DELETE', 'OPTIONS'],
